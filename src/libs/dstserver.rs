959809a78c64b964c162d799c40f40ca7f83dd67
azureqaq <azureqaq@gmail.com>
2022-12-06 13:39:21 +0800

    update dstserver

---
+++ src/libs/dstserver.rs
@@ -0,0 +1,32 @@
+//! DSTSERVER Related Modules
+
+use anyhow::Result;
+use std::{fs::read_to_string, path::Path};
+
+pub enum DstVersion {
+    Release(u32),
+    Test(u32),
+    /// Unknow yet
+    Unknow(u32),
+}
+
+pub trait DstServer {
+    /// the version info of `DstServer`
+    fn dstversion(&self) -> Result<DstVersion> {
+        let content = read_to_string(self.version_txt_path())?;
+        let num: u32 = content.trim().parse()?;
+        Ok(DstVersion::Unknow(num))
+    }
+
+    /// the bin_32 path
+    fn bin_32(&self) -> &Path;
+
+    /// the bin_64 path
+    fn bin_64(&self) -> &Path;
+
+    /// version.txt file_path
+    fn version_txt_path(&self) -> &Path;
+
+    /// folder (where to install dst)
+    fn folder(&self) -> &Path;
+}
--- src/libs/mod.rs
+++ src/libs/mod.rs
@@ -1,1 +1,1 @@
-
+pub mod dstserver;
